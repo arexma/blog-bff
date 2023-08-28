d4972db132566e9e2ac6090c66d38dfd26370522
Alexander Ma <maalexander2@yahoo.com>
2023-08-28 05:22:28 -0700

    Testing firebase for chat implementation, added some config for ts, switched to yarn

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,12 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import * as admin from 'firebase-admin';
 
 export const app: Application = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-
-const serviceAccount = require('../gatchagameblog-firebase-adminsdk-8darc-8ce3094eb0.json');
-admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
-
-export const db = admin.firestore();
 
 const PORT = process.env.PORT || 8000;
 
