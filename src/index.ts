6ab4384e07159f58d17e2df1ef9d1585a404a93b
Alexander Ma <maalexander2@yahoo.com>
2023-08-30 04:37:04 -0700

    Fixed some config for nodemon/ts-node, initial setup for Express/Socket.IO

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import routes from './routes/index';
 
 export const app: Application = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+app.use('/', routes);
 
 const PORT = process.env.PORT || 8000;
 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,12 @@
 import { app } from '../index';
-import { Request, Response } from 'express';
+import express, { Request, Response } from 'express';
 
-app.get('/health-check', (req: Request, res: Response) => {
+const router = express.Router();
+
+router.get('/health-check', (req: Request, res: Response) => {
   res.send('BFF is healthy!');
 });
+
+
+export default router;
 
